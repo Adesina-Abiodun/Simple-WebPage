4297c8fcd4f5537e82a302330df71f9fb5da5a91
Adesina-Abiodun <101020483+Adesina-Abiodun@users.noreply.github.com>
2022-10-25 16:23:35 +0100

    Linked Buttons Created

--- task-1/src/Components/WebPage/WebPage.js
+++ task-1/src/Components/WebPage/WebPage.js
@@ -6,6 +6,40 @@ import Arrow from '../../Assets/Icons/Arrow.svg'
 import Dots from '../../Assets/Icons/Dotted.svg'
 
  export const WebPage = () => {
+
+                // BUTTON ARRAY
+    const Btns = [{
+                    Name: "Twitter Link",
+                    id: 'twitter_btn',
+                    link: "https://mobile.twitter.com/Adesinaabiodunp"
+                },
+                {
+                    Name: "Zuri Team",
+                    id: 'btn_zuri',
+                    link: "https://training.zuri.team/"
+                },
+                {
+                    Name: "Zuri Books",
+                    id: 'books',
+                    link: "https://books.zuri.team"
+                },
+                {
+                    Name: "Python Books",
+                    id: 'book_python',
+                    link: "https://books.zuri.team/python-for-beginners?ref_id=adesinaabiodunpaul"
+                },
+                {
+                    Name: "Background Checks for Codes",
+                    id: 'pitch',
+                    link: "https://background.zuri.team"
+                },
+                {
+                    Name: "Design Books",
+                    id: 'book_design',
+                    link: "https://books.zuri.team/design-rules"
+                }]
+
+
     return ( 
         <section id="container">
                                     {/* Main-Section */}
@@ -22,6 +56,14 @@ import Dots from '../../Assets/Icons/Dotted.svg'
                         <img   src={Arrow} alt="Arrow"  className='Arrow'/>
                         <img src={Dots} alt="Hidden list" className='Dots' />
                     </header>
+
+                     {/* Mapping through the Array */}
+
+                    {Btns.map((btn) => {
+                    return <div className='btns' key={btn.id}>
+                        <button id={btn.id}><a href={btn.link} target='blank'>{btn.Name}</a></button>
+                    </div>
+                    })}
 
                 </body>
 
