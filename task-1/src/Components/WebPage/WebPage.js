a4b587f933be2d89ffb6dc7ff2bc6a8cb1e00240
Adesina-Abiodun <101020483+Adesina-Abiodun@users.noreply.github.com>
2022-10-25 16:14:21 +0100

    User Profile created

--- task-1/src/Components/WebPage/WebPage.js
+++ task-1/src/Components/WebPage/WebPage.js
@@ -1,10 +1,31 @@
+import './WebPage.css'
+import "@fontsource/inter"
 
+import User from '../../Assets/round1.png'
+import Arrow from '../../Assets/Icons/Arrow.svg'
+import Dots from '../../Assets/Icons/Dotted.svg'
 
  export const WebPage = () => {
     return ( 
-        <div>
+        <section id="container">
+                                    {/* Main-Section */}
+            <body className="Main-section">
 
-        </div>
+                    {/* User Image and Username */}
+                
+                    <header>
+                        <div className='UserProfile'>
+                        <img src={User} alt="UserImage" id='profile_img' />
+                        <p id='twitter'>Adesina Abiodun Paul</p>
+                        <p id='slack'>Adesina Abiodun P.</p>
+                        </div>
+                        <img   src={Arrow} alt="Arrow"  className='Arrow'/>
+                        <img src={Dots} alt="Hidden list" className='Dots' />
+                    </header>
+
+                </body>
+
+        </section>
      );
 }
  
